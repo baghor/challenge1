bf3aeb1e594e0889e8dcbed97c5adf78b8ec0541
baghor <inezairene30@gmail.com>
2021-07-02 16:36:50 +0200

    implemented the fetch nuser functions and displayed user/ created get posts fucntions to get posts of all users

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ function fetchData() {
         return data = response.json();
     }).then(data => {
         // console.log(data);
+        // appendData(data);
         const html = data.map(user => {
             return `
                 <div class="user">
-                    <div class="d-flex">
+                    <div class="">
                         <div>
                             <p>Name: ${user.name}</p>
                             <p>Email: ${user.email}</p>
                         </div>
                         <div>
-                            <button class="button">Get User's Posts</button>
-                        </div>
+                            <button onClick="fetchPosts(${user.id})" class="btn btn-primary">Get User's Posts</button>
+                        </div>  
                     </div>
                 </div>
             `
@@ -28,6 +29,61 @@ function fetchData() {
     }).catch(error => {
         console.log(error);
     });
+
+
 }
 
 fetchData();
+
+// function myFunction() {
+//     // document.getElementById("demo").innerHTML = "Hello World";
+// }
+
+function fetchPosts(userId) {
+    fetch('https://jsonplaceholder.typicode.com/posts?userId=' + userId).then(response => {
+        if (!response.ok) {
+            throw error('ERROR');
+        }
+
+        return data = response.json();
+    }).then(data => {
+        const posts = data.map(post => {
+            return `
+            <div>
+                <p class="font-weight-bold">Title:<br> <span>${post.title}</span> </p>
+                <p class="font-weight-bold">Body: <br> <span>${post.body}</span></p>
+            </div>`
+        });
+        document
+            .querySelector('#posts')
+            .insertAdjacentHTML('afterbegin', posts);
+        console.log(data)
+    }).catch(error => {
+        console.log(error);
+    });
+
+
+}
+
+// const fetchDataBtn = document.getElementById('fetchdata')
+// const result = document.getElementById('result')
+
+// // gets data from API and sets the content of #result div
+// const getData = function () {
+//     result.innerText = 'Loading....'
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//             result.innerText = JSON.stringify(data, null, 2)
+//         })
+//         .catch(error => console.log(error))
+// }
+
+// // add event listener for #fetchdata button
+// if (fetchDataBtn) {
+//     fetchDataBtn.addEventListener('click', getData);
+// }
+// fetchDataBtn.addEventListener('click', getData)
+
+fetchPosts();
